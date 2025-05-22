1fd6619f683e43ec2e9ff1606efa5d1077012baa
Deepika-tech-dev <deepswarnstudy777@gmail.com>
2025-05-22 10:51:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with todo app
         </a>
       </header>
     </div>
